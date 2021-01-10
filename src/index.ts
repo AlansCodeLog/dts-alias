36ad9529c60651360326b5b9ef6447a4b4b97df6
Alan <alanscodelog@gmail.com>
2021-01-09 23:46:27 -0300

    deps: updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -12,67 +12,67 @@ const exec = util.promisify(_exec)
 
 
 async function main(): Promise<void> {
-	let args = process.argv.slice(2).join(" ")
+	const args = process.argv.slice(2).join(" ")
 	if (process.env.DEBUG) console.log(`Arguments passed to tsc: ${args}`)
 
-	let command = `tsc --showConfig ${args}`
+	const command = `tsc --showConfig ${args}`
 
-	let { stdout, stderr } = await exec(command)
+	const { stdout, stderr } = await exec(command)
 	if (stderr) {
 		console.log(stderr)
 		process.exit(1)
 	}
 
-	let tsconfig = JSON.parse(stdout)
+	const tsconfig = JSON.parse(stdout)
 	if (process.env.DEBUG) console.log(`tsconfig:\n${pretty(tsconfig)}`)
 
-	let out_dir = path.resolve(tsconfig.compilerOptions.outDir || "dist")
-	if (process.env.DEBUG) console.log(`outDir: ${out_dir}`)
-	let root_dir = path.resolve(tsconfig.compilerOptions.rootDir || "src")
-	if (process.env.DEBUG) console.log(`rootDir: ${root_dir}`)
-	let base_url = tsconfig.compilerOptions.baseUrl || "."
-	if (process.env.DEBUG) console.log(`baseUrl: ${base_url}`)
+	const outDir = path.resolve(tsconfig.compilerOptions.outDir || "dist")
+	if (process.env.DEBUG) console.log(`outDir: ${outDir}`)
+	const rootDir = path.resolve(tsconfig.compilerOptions.rootDir || "src")
+	if (process.env.DEBUG) console.log(`rootDir: ${rootDir}`)
+	const baseUrl = tsconfig.compilerOptions.baseUrl || "."
+	if (process.env.DEBUG) console.log(`baseUrl: ${baseUrl}`)
 
-	let paths: {path: string, regex: RegExp, get_path: (part: string) => string}[] = []
+	const paths: {path: string, regex: RegExp, getPath: (part: string) => string}[] = []
 
-	for (let path_string of Object.keys(tsconfig.compilerOptions.paths)) {
-		let path_parts = path_string.split("*")
-		let start = escape(path_parts[0])
-		let end = escape(path_parts[1])
+	for (const pathString of Object.keys(tsconfig.compilerOptions.paths)) {
+		const pathParts = pathString.split("*")
+		const start = escape(pathParts[0])
+		const end = escape(pathParts[1])
 
 
-		let regex = `(import .*? ('|"))${start}(.*?)(${end}\\2)`
+		const regex = `(import .*? ('|"))${start}(.*?)(${end}\\2)`
 		paths.push({
-			path: path_string,
+			path: pathString,
 			regex: new RegExp(regex, "g"),
-			get_path: (part: string) => {
-				let raw_path = tsconfig.compilerOptions.paths[path_string][0].replace("*", part)
-				let parts = path.normalize(raw_path).split(path.sep)
+			getPath: (part: string) => {
+				let rawPath = tsconfig.compilerOptions.paths[pathString][0].replace("*", part)
+				const parts = path.normalize(rawPath).split(path.sep)
 
-				let path_root_dir = parts[0]
-				if (path.resolve(path_root_dir) === root_dir) {
-					raw_path = path.join(out_dir, ...parts.slice(1))
+				const pathRootDir = parts[0]
+				if (path.resolve(pathRootDir) === rootDir) {
+					rawPath = path.join(outDir, ...parts.slice(1))
 				}
-				return raw_path
+				return rawPath
 			},
 		})
 	}
 
-	let glob_str = `${out_dir.replace(/\\/g, "/")}/**/*.d.ts`
-	if (process.env.DEBUG) console.log(`glob: ${glob_str}`)
+	const globStr = `${outDir.replace(/\\/g, "/")}/**/*.d.ts`
+	if (process.env.DEBUG) console.log(`glob: ${globStr}`)
 
-	let files = await glob(glob_str)
+	const files = await glob(globStr)
 	if (process.env.DEBUG) console.log(`files:\n\t${files.join("\n\t")}`)
 	await Promise.all(files.map(async file => {
 		let contents = (await fs.readFile(file)).toString()
-		let changes: string[][] = []
-		for (let entry of paths) {
+		const changes: string[][] = []
+		for (const entry of paths) {
 			contents = contents.replace(entry.regex, (
 				match: string, start: string, _quotes: string, str: string, end: string
 			): string => {
-				let real_loc = path.resolve(base_url, entry.get_path(str))
+				const realLoc = path.resolve(baseUrl, entry.getPath(str))
 
-				let relative = path.relative(path.dirname(file), real_loc).replace(/\\/g, "/") + end
+				let relative = path.relative(path.dirname(file), realLoc).replace(/\\/g, "/") + end
 				if (!relative.startsWith("../")) relative = `./${relative}`
 
 				changes.push([match, start + relative])
@@ -82,7 +82,7 @@ async function main(): Promise<void> {
 		if (process.env.DEBUG) {
 			if (changes.length > 0) console.log(`Fixed aliases in ${file}:`)
 
-			for (let change of changes) {
+			for (const change of changes) {
 				console.log(`\tChanged:  \`${change[0]}\`\n\tTo:       \`${change[1]}\``)
 			}
 		}
